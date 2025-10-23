d1d215cf80899b282b2c2df6eea74c1ce8d5efcc
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-10-22 23:18:38 -0300

    Criada a classe, interface e metodo de validação de login

---
+++ QuestionsProject/QuestionsSolution/Validators/IValidators/IUserValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionsSolution.Validators.IValidators
+{
+    interface IUserValidator
+    {
+        public bool ValidateLogin(string login);
+
+    }
+}
---
+++ QuestionsProject/QuestionsSolution/Validators/UserValidator.cs
@@ -0,0 +1,31 @@
+﻿using QuestionsSolution.Validators.IValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionsSolution.Validators
+{
+    class UserValidator : IUserValidator
+    {
+        private IUserValidator _userValidator;
+
+        public UserValidator(IUserValidator userValidator)
+        {
+            _userValidator = userValidator;
+        }
+
+        public bool ValidateLogin(string login)
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrWhiteSpace(login))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
