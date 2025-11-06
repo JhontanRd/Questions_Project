cd0e07ae0738bef46d4372202bddfb20c82f9cf2
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-11-05 22:29:05 -0300

    Criado namespace Services e a classe HashGenerator

--- QuestionsProject/QuestionsSolution/Models/User.cs
+++ QuestionsProject/QuestionsSolution/Models/User.cs
@@ -1,10 +1,21 @@
-﻿namespace QuestionsSolution.Models
+﻿using QuestionsSolution.Services;
+
+namespace QuestionsSolution.Models
 {
     class User
     {
+        private string _userPassword;
         public int UserId { get; private set; }
         public string UserLogin { get; private set; }
-        public string UserPassword { get; private set; }
+
+        public string UserPassword
+        {
+            get { return _userPassword; }
+            set
+            {
+                _userPassword = HashGenerator.GetHash(value);
+            }
+        }
 
         public User(string userLogin, string userPassword)
         {
@@ -17,6 +28,6 @@
         {
             return "User created";
         }
-        
+
     }
 }
---
+++ QuestionsProject/QuestionsSolution/Services/HashGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+namespace QuestionsSolution.Services
+{
+    public static class HashGenerator
+    {
+        public static string GetHash(string value)
+        {
+            // Aqui eu converto a string para bytes (UTF8)
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+
+            // Criar o algoritmo SHA256 que gera um hash de 64 caracteres hexadecimais
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Gerando o hash
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                // Converter e retorna o hash em string hexadecimal legível
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+            }
+        }
+    }
+}
